29506c9419e3a7d675463e242351b89b41eb06e6
Noor Clean <noorclean98@gmail.com>
2021-02-10 20:37:07 +0700

    add functionality for search term, sort by and limit value

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
 searchForm.addEventListener('submit', e => {
+    //Get search term 
+    const searchTerm = searchInput.value;
+    // Get sort
+    const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+    // Get limit
+    const searchLimit = document.getElementById('limit').value;
+
     
 
     e.preventDefault();
