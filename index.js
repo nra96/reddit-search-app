c14fb8542ede634a9456b3514dd15b8c3801d477
Noor Clean <noorclean98@gmail.com>
2021-02-10 22:10:15 +0700

    add link, subreddit and score ssection

--- index.js
+++ index.js
@@ -35,7 +35,14 @@ searchForm.addEventListener('submit', e => {
             <div class="card-body">
               <h5 class="card-title">${post.title}</h5>
               <p class="card-text">${truncateText(post.selftext, 100)}</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <a href="${post.url}" target="_blank" class="btn btn-primary">Read More</a>
+              <hr>
+              <span class="badge badge-secondary">Subreddit: 
+               ${post.subreddit}
+              </span>
+              <span class="badge badge-dark">Score: 
+               ${post.score}
+              </span>
             </div>
           </div>
             `;
