fa8f207badb6e263fe36e506d6354612dd84f908
Noor Clean <noorclean98@gmail.com>
2021-02-10 20:49:02 +0700

    add show message functionality

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ searchForm.addEventListener('submit', e => {
     const searchLimit = document.getElementById('limit').value;
 
     // Check input
-    if(searchTerm === ''){
-    // Show message
-    showMessage('Please add a search term', 'alert-danger');
+    if (searchTerm === '') {
+        // Show message
+        showMessage('Please add a search term', 'alert-danger');
     }
 
-e.preventDefault();
+    e.preventDefault();
 });
+
+//Show message
+function showMessage(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add Text 
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const searchContainer = document.getElementById('search-container');
+    // Get search
+    const search = document.getElementById('search');
+
+    //insert message 
+    searchContainer.insertBefore(div, search);
+
+    // Timeout alert
+    setTimeout(() => document.querySelector('alert').remove(), 3000);
+}
